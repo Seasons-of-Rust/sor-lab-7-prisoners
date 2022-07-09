bfd7aeb62256769418f51e9da879812169e1d4e3
Jersey Aubin-Déry <k.aubindery@gmail.com>
2022-07-09 16:13:11 -0400

    Update src/bots/joss.rs
    Co-authored-by: Forest Anderson <forestkzanderson@gmail.com>

--- src/bots/joss.rs
+++ src/bots/joss.rs
@@ -12,9 +12,9 @@ impl Bot for Joss {
 
     fn turn(&mut self, history: &[Turn]) -> Dilemma {
         let mut rng = rand::thread_rng();
-        match rng.gen_range(0..10) {
+        match rng.gen_range(0i32..10) {
             // 10% of the time, betray
-            0i32 => Dilemma::Betray,
+            0 => Dilemma::Betray,
             // 90% of the time, use tft
             _ => {
                 history
